473d0ffe4cf3deaf0e511a0a7a97721617a8999c
Kike <elviejokike@hotmail.com>
2018-02-16 21:57:54 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 from setuptools import setup, find_packages
 
-
-try:
-    from ecsspawner import __about__
-    about = __about__.__dict__
-except ImportError:
-    # installing - dependencies are not there yet
-    # Manually extract the __about__
-    about = dict()
-    exec(open("about/__about__.py").read(), about)
-
 setup(
     name='ecsspawner',
-    version = about['__version__'],
-    author = about['__author__'],
-    url=about['__url__'],
-    license="Apache License 2.0",
+    version = '0.0.1',
+    author ='elviejokike',
+    license="MIT",
     author_email = 'kike@world.com',
     description = 'ECS/AWS Spawner for Jupyter Hub',
     keywords = 'ECS AWS Jupyter Hub',
